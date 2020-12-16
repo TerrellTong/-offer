62bae149aa07c1ca76f987548770940d6970e402
TerrellTong <terrell_tong@126.com>
2020-12-16 19:45:37 +0800

    JZ7 斐波拉契数列.java

---
+++ JZ7 斐波拉契数列.java
@@ -0,0 +1,19 @@
+/*
+	算法思想：
+		简单的动态规划
+*/
+
+
+public class Solution {
+    public int Fibonacci(int n) {
+        if(n == 0 || n == 1)
+             return n;
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+}
