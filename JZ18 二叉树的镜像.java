638dfa2e6c60f6a1c6d6c8608a360b87752e6349
TerrellTong <terrell_tong@126.com>
2021-01-12 19:26:08 +0800

    JZ18 二叉树的镜像.java

---
+++ JZ18 二叉树的镜像.java
@@ -0,0 +1,32 @@
+/*
+	算法思想：
+		简单的递归！
+*/
+
+
+
+/**
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+public class Solution {
+    public void Mirror(TreeNode root) {
+        if(root == null)
+            return;
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        
+        Mirror(root.left);
+        Mirror(root.right);
+    }
+}
